0a0144df23f68a01e39f7b6de89bc864cf5552f6
ehsan.panahy <ehsan.panahy@gmail.com>
2018-07-15 18:03:50 +0430

    Angular Assignment3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatSliderModule} from '@angular/material/slider';
 
 
 import 'hammerjs';
@@ -73,7 +74,8 @@ import { LoginComponent } from './login/login.component';
     ReactiveFormsModule,
     MatSelectModule,
     MatSlideToggleModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSliderModule
   ],
   providers: [ DishService,
      PromotionService, LeaderService],
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,9 +4,14 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService} from '../services/dish.service';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { switchMap } from 'rxjs/operators';
 
 import {Dish} from '../shared/dish';
+
+import { Comment } from '../shared/comment';
+
 
 @Component({
   selector: 'app-dishdetail',
@@ -15,15 +20,90 @@ import {Dish} from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
   
-  
+    commentFlag: number;
+    commentForm:FormGroup;
+    comment: Comment;
     dish: Dish;
     dishIds: number[];
     prev: number;
     next: number;
 
+    formErrors = {
+      'author': '',
+      'comment': ''
+    };
+
+    validationMessages = {
+      'author': {
+        'required':     ' The Name field is required.',
+        'minlength':    ' First Name must be at least 2 characters long.',
+
+      },
+      'comment': {
+        'required':     ' The Comment field is required.',
+      }
+    };
+
     constructor(private dishService: DishService, 
       private route: ActivatedRoute,
-      private location: Location) { }
+      private location: Location,
+      private fb: FormBuilder) {
+        this.createForm();
+
+       }
+
+       createForm(): void {
+        this.commentForm = this.fb.group({
+          author:['',[Validators.required, Validators.minLength(2)]],
+          rating:5,
+          comment:['', Validators.required]
+
+        });
+
+        this.commentForm.valueChanges
+          .subscribe(data => this.onValueChanged(data));
+
+        this.onValueChanged();
+       }
+
+       onValueChanged(data?: any){
+
+        this.comment = this.commentForm.value;
+        //this.comment.rating = this.commentForm.get('rating').value;
+        const newDate = new Date(Date.now());
+        this.comment.date = newDate.toISOString() ;
+
+        if(!this.commentForm) {return;}
+        const form = this.commentForm;
+
+        if (form.valid){
+          this.commentFlag = 1;
+        }
+        
+        for (const field in this.formErrors){
+          this.formErrors[field] = '';
+          const control = form.get(field);
+          if (control && control.dirty  && !control.valid){
+            
+            const messages = this.validationMessages[field];
+            for (const key in control.errors){
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+       };
+
+       onSubmit() {
+
+        this.dish.comments.push(this.comment);
+        this.commentFlag = 0;
+        console.log(this.comment);
+        this.commentForm.reset ({
+          author: '',
+          rating:5,
+          comment: ''
+        });
+      }
 
   ngOnInit() {
     this.dishService.getDishId()
@@ -48,5 +128,7 @@ export class DishdetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+
 
 }
