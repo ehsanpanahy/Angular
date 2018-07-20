3e2eede90f46f993dedb4589aa72f3d4e2fa64ff
ehsan.panahy <ehsan.panahy@gmail.com>
2018-07-20 13:38:38 +0430

    Angular Animation Part1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,7 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService} from '../services/dish.service';
 
+import { trigger, state, style, animate, transition, animation} from '@angular/animations';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { switchMap } from 'rxjs/operators';
@@ -16,7 +17,20 @@ import { Comment } from '../shared/comment';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility',[
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1  
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
   
@@ -29,7 +43,7 @@ export class DishdetailComponent implements OnInit {
     prev: number;
     next: number;
     errMess: string;
-
+    visibility = 'shown';
     formErrors = {
       'author': '',
       'comment': ''
@@ -116,11 +130,15 @@ export class DishdetailComponent implements OnInit {
       .subscribe(dishIds => this.dishIds = dishIds);
 
     this.route.params
-      .pipe(switchMap((params: Params) => this.dishService.getDish(+params['id'])))
+      .pipe(switchMap((params: Params) => {
+        this.visibility='hidden'; 
+        return this.dishService.getDish(+params['id']);
+      }))
       .subscribe(dish => {
         this.dish = dish;
         this.dishCopy = dish;
         this.setPrevNext(dish.id);
+        this.visibility = 'shown';
         },
         errmess => this.errMess = <any>errmess);
   }
